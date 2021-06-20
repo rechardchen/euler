307e0461f73da9a6683e2337b31cfa2f59a81d89
rechardchen <rechard_chen@qq.com>
2021-06-20 23:30:19 +0800

    base&input

---
+++ core/base.py
@@ -0,0 +1,61 @@
+import glfw
+import glfw.GLFW
+from input import Input
+
+class Base(object):
+    def __init__(self, windowSize = [512,512], windowTitle="Euler Framework") -> None:
+        glfw.init()
+        glfw.window_hint(glfw.GLFW.GLFW_CONTEXT_VERSION_MAJOR, 4)
+        glfw.window_hint(glfw.GLFW.GLFW_CONTEXT_VERSION_MINOR, 3)
+        glfw.window_hint(glfw.GLFW.GLFW_OPENGL_PROFILE, glfw.GLFW.GLFW_OPENGL_CORE_PROFILE)
+
+        self.window = glfw.create_window(windowSize[0], windowSize[1], windowTitle, None, None)
+
+        #make the window context current
+        glfw.make_context_current(self.window)
+
+        self.input = Input()
+        glfw.set_key_callback(self.window, self.windowKeyCallback)
+
+    # implement by extending class
+    def initialize(self):
+        pass
+    
+    # implement by extending class
+    def update(self):
+        pass
+
+    def run(self):
+        # start up
+        self.initialize()
+
+        while not glfw.window_should_close(self.window):
+            ## process input ##
+            glfw.poll_events()
+            self.input.update()
+
+            ## update & render ##
+            self.update()
+
+            glfw.swap_buffers(self.window)
+
+        glfw.terminate()
+    
+    def windowKeyCallback(self, window, key, scanCode, action, mode):
+        self.input.receiveKeyEvent(key,action,mode)
+
+if __name__ == "__main__":
+    # Base().run()
+
+    class Test(Base):
+        def update(self):
+            if self.input.keyDownList or self.input.keyPressedList or self.input.keyUpList:
+                print("==========================================================")
+                if len(self.input.keyDownList) > 0:
+                    print("keys down: ", self.input.keyDownList)
+                if len(self.input.keyPressedList) > 0:
+                    print("keys pressed: ", self.input.keyPressedList)
+                if len(self.input.keyUpList) > 0:
+                    print("keys up: ", self.input.keyUpList)
+    
+    Test().run()
---
+++ core/input.py
@@ -0,0 +1,41 @@
+import glfw.GLFW
+
+class KeyEvent:
+    def __init__(self,key,action,mode):
+        self.key = key
+        self.action = action
+        self.mode = mode
+
+class Input(object):
+    def __init__(self) -> None:
+        self.keyDownList    = set()
+        self.keyUpList      = set()
+        self.keyPressedList = set()
+
+        self.keyEvents      = []
+
+    def update(self):
+        self.keyDownList.clear()
+        self.keyUpList.clear()
+
+        for evt in self.keyEvents:
+            if evt.action == glfw.GLFW.GLFW_PRESS:
+                self.keyDownList.add(evt.key)
+                self.keyPressedList.add(evt.key)
+            elif evt.action == glfw.GLFW.GLFW_RELEASE:
+                self.keyUpList.add(evt.key)
+                self.keyPressedList.remove(evt.key)
+        self.keyEvents.clear()
+
+    def isKeyDown(self,key):
+        return key in self.keyDownList
+    
+    def isKeyUp(self, key):
+        return key in self.keyUpList
+    
+    def isKeyPressed(self, key):
+        return key in self.keyPressedList
+
+    def receiveKeyEvent(self, k,a,m):
+        self.keyEvents.append(KeyEvent(k,a,m))
+    
